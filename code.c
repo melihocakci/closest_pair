65a0d33b19b178f3289b61b84af086d1cbf850f4
Melih Ocakcı <melihocakci@gmail.com>
2021-11-17 20:10:44 +0300

    final

--- code.c
+++ code.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// noktalar için struct
 typedef struct POINT
 {
     int x;
     int y;
 } point;
 
+// nokta çiftleri için struct
 typedef struct PAIR
 {
     point p1;
     point p2;
-    int dist;
+    int dist; // mesafenin karesi
 } pair;
 
+// kıyaslama için iki nokta arası mesafe hesaplama algoritması
 int calc_dist(point p1, point p2)
 {
     return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
 }
 
-void quicksort(point *number, int first, int last)
+// y eksenine göre sıralama
+void quicksort_y(point *list, int first, int last)
 {
     int i, j, pivot;
-    point temp;
+    point tmp;
 
     if (first < last)
     {
@@ -32,40 +36,75 @@ void quicksort(point *number, int first, int last)
 
         while (i < j)
         {
-            while (number[i].y <= number[pivot].y && i < last)
+            while (list[i].y <= list[pivot].y && i < last)
                 i++;
-            while (number[j].y > number[pivot].y)
+            while (list[j].y > list[pivot].y)
                 j--;
             if (i < j)
             {
-                temp = number[i];
-                number[i] = number[j];
-                number[j] = temp;
+                tmp = list[i];
+                list[i] = list[j];
+                list[j] = tmp;
             }
         }
 
-        temp = number[pivot];
-        number[pivot] = number[j];
-        number[j] = temp;
-        quicksort(number, first, j - 1);
-        quicksort(number, j + 1, last);
+        tmp = list[pivot];
+        list[pivot] = list[j];
+        list[j] = tmp;
+        quicksort_y(list, first, j - 1);
+        quicksort_y(list, j + 1, last);
     }
 }
 
+// x eksenine göre sıralama
+void quicksort_x(point *list, int first, int last)
+{
+    int i, j, pivot;
+    point tmp;
+
+    if (first < last)
+    {
+        pivot = first;
+        i = first;
+        j = last;
+
+        while (i < j)
+        {
+            while (list[i].x <= list[pivot].x && i < last)
+                i++;
+            while (list[j].x > list[pivot].x)
+                j--;
+            if (i < j)
+            {
+                tmp = list[i];
+                list[i] = list[j];
+                list[j] = tmp;
+            }
+        }
+
+        tmp = list[pivot];
+        list[pivot] = list[j];
+        list[j] = tmp;
+        quicksort_x(list, first, j - 1);
+        quicksort_x(list, j + 1, last);
+    }
+}
+
+// nokta listesi, listenin başlangıç ve bitiş indislerini alıp en yakın nokta çiftini döndürür
 pair find_dmin(point *list, int min, int max)
 {
-    pair dmin;
-    if (max - min == 1)
+    pair dmin;          // en yakın çift
+    if (max - min == 1) // 2 nokta kalma durumu
     {
         dmin.p1 = list[min];
         dmin.p2 = list[max];
         dmin.dist = calc_dist(dmin.p1, dmin.p2);
     }
-    else if (max - min == 2)
+    else if (max - min == 2) // 3 nokta kalma durumu
     {
-        int dist1 = calc_dist(list[min], list[min + 1]);
-        int dist2 = calc_dist(list[min + 1], list[min + 2]);
-        int dist3 = calc_dist(list[min + 2], list[min]);
+        int dist1 = calc_dist(list[min], list[min + 1]);     // 1. çift
+        int dist2 = calc_dist(list[min + 1], list[min + 2]); // 2. çift
+        int dist3 = calc_dist(list[min + 2], list[min]);     // 3. çift
 
         if (dist1 < dist2 && dist1 < dist3)
         {
@@ -86,12 +125,14 @@ pair find_dmin(point *list, int min, int max)
             dmin.p2 = list[min + 2];
         }
     }
-    else
+    else // 4 ya da daha fazla nokta kalma durumu
     {
+        // fonksiyonun rekürsif olarak çağrılması
         int mid = min + (max - min) / 2;
-        pair dl = find_dmin(list, min, mid);
-        pair dr = find_dmin(list, mid + 1, max);
+        pair dl = find_dmin(list, min, mid);     // sol tarafın en yakın çifti
+        pair dr = find_dmin(list, mid + 1, max); // sağ tarafın en yakın çifti
 
+        // sol ve sağ dmin karşılaştırması
         if (dl.dist < dr.dist)
         {
             dmin = dl;
@@ -101,8 +142,9 @@ pair find_dmin(point *list, int min, int max)
             dmin = dr;
         }
 
+        // merkeze yakın olan noktaların listesinin çıkartılması
         int i, j, k = 0;
-        point *arr = (point *)malloc(sizeof(point) * (max - min + 1));
+        point *arr = (point *)malloc(sizeof(point) * (max - min + 1)); // merkeze yakın olan noktaların listesi
         for (i = min; i <= max; i++)
         {
             if ((list[i].x - list[mid].x) * (list[i].x - list[mid].x) <= dmin.dist)
@@ -112,8 +154,9 @@ pair find_dmin(point *list, int min, int max)
             }
         }
 
-        quicksort(arr, 0, k - 1);
+        quicksort_y(arr, 0, k - 1);
 
+        // dmin'den küçük olan çiftlerin saptanması
         for (i = 0; i < k - 1; i++)
         {
             j = i + 1;
@@ -135,15 +178,33 @@ pair find_dmin(point *list, int min, int max)
 
 int main()
 {
-    FILE *fp = fopen("sample.txt", "r");
-    int i;
-    point list[10];
-    for (i = 0; i < 10; i++)
+    // sample.txt dosyasını açma
+    char *dosya = (char *)malloc(sizeof(char) * 20);
+    printf("Degerlerin alinacagi dosya adi: ");
+    scanf("%s", dosya);
+    FILE *fp = fopen(dosya, "r");
+    if (fp == NULL)
+    {
+        printf("Dosya bulunamadi");
+        return 0;
+    }
+
+    // dosyadan isteye kaydetme
+    int i, k;
+    printf("Alinacak koordinat sayisi: ");
+    scanf("%d", &k);
+    point *list = (point *)malloc(sizeof(point) * k); // nokta listesi
+    for (i = 0; i < k; i++)
     {
         fscanf(fp, "%d %d", &list[i].x, &list[i].y);
     }
+    quicksort_x(list, 0, k - 1);
 
-    pair dmin = find_dmin(list, 0, 9);
+    // dmin bulma
+    pair dmin = find_dmin(list, 0, k - 1); // en yakın çift
 
-    printf("%d %d\n%d %d", dmin.p1.x, dmin.p1.y, dmin.p2.x, dmin.p2.y);
+    free(list);
+    close(fp);
+    // sonucu yazdırma
+    printf("En yakin ikili:\n(%d,%d)\n(%d,%d)", dmin.p1.x, dmin.p1.y, dmin.p2.x, dmin.p2.y);
 }
